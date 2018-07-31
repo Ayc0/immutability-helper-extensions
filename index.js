8d24ba671e265d0008efd7152188e9a35c948e1e
Ayc0 <ayc0.benj@gmail.com>
2018-07-31 17:54:14 +0200

    add delete* in index

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const find = require("./internals/find");
 const del = require("./internals/delete");
 const insertAfter = require("./internals/insertAfter");
 const insertBefore = require("./internals/insertBefore");
+const deleteAfter = require("./internals/deleteAfter");
+const deleteBefore = require("./internals/deleteBefore");
 
 update.extend("$find", find);
-
 update.extend("$delete", del);
-
 update.extend("$insertAfter", insertAfter);
-
 update.extend("$insertBefore", insertBefore);
+update.extend("$deleteAfter", deleteAfter);
+update.extend("$deleteBefore", deleteBefore);
 
 export default update;
